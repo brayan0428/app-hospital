00a3106556baa55575b4100a934d3071da8b33c4
Brayan Llanos <brayan0428@gmail.com>
2020-06-03 22:22:02 -0500

    Se creo un modulos para las pages y uno para los shareds, se creo un archivo de routing para las pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,32 +5,17 @@ import { NgModule } from "@angular/core";
 // tslint:disable-next-line: quotemark
 import { AppComponent } from "./app.component";
 import { LoginComponent } from "./login/login.component";
-import { NopagefoundComponent } from "./shared/nopagefound/nopagefound.component";
-import { DashboardComponent } from "./pages/dashboard/dashboard.component";
-import { ProgressComponent } from "./pages/progress/progress.component";
-import { Graficas1Component } from "./pages/graficas1/graficas1.component";
-import { HeaderComponent } from "./shared/header/header.component";
-import { SidebarComponent } from "./shared/sidebar/sidebar.component";
-import { BreadcrumbsComponent } from "./shared/breadcrumbs/breadcrumbs.component";
-import { APP_ROUTES } from './app.routing';
-import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './login/register.component';
+import { PagesModule } from './pages/pages.module';
 
+import { APP_ROUTES } from './app.routing';
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    NopagefoundComponent,
-    DashboardComponent,
-    ProgressComponent,
-    Graficas1Component,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbsComponent,
-    PagesComponent,
     RegisterComponent,
   ],
-  imports: [BrowserModule, APP_ROUTES],
+  imports: [BrowserModule, APP_ROUTES, PagesModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,23 +1,8 @@
 import { Routes, RouterModule } from "@angular/router"
-import { DashboardComponent } from './pages/dashboard/dashboard.component'
-import { PagesComponent } from './pages/pages.component'
 import { LoginComponent } from './login/login.component'
 import { RegisterComponent } from './login/register.component'
 
 const appRoutes:Routes = [
-  {
-    path: '',
-    component: PagesComponent,
-    children: [
-      {
-        path: 'dashboard',
-        component: DashboardComponent
-      },
-      {
-        path: '', redirectTo: '/dashboard', pathMatch:'full'
-      }
-    ]
-  },
   {
     path: 'login',
     component: LoginComponent
